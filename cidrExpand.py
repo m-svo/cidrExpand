8ba1da6b0d8c8b7b5fa11151f6e228448de4f4d4
omicron-b <linuxgaming@protonmail.com>
2020-03-04 07:45:35 +0000

    - port to python3

--- cidrExpand.py
+++ cidrExpand.py
@@ -1,17 +1,14 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #Jeremy Galloway // root at cypherg dot com
 
 from netaddr import IPNetwork
 import sys
 
 if len(sys.argv) < 2:
-	print 'example usage: python cidrExpand.py cidrRanges.txt >> output.txt'
-	
+	print ('example usage: python3 cidrExpand.py cidrRanges.txt >> output.txt')
+
 with open(sys.argv[1], 'r') as cidrRanges:
 	for line in cidrRanges:
 		ip = IPNetwork(line)
 		for ip in ip:
-			print ip
-
-cidrRanges.close()
-exit()
+			print (ip)
